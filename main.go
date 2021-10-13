67f1df97332586e8a15bdf7178c23f260bbf37dd
Andrew Woodruff <agwoodruff93@gmail.com>
2021-10-13 14:04:45 -0600

    add logging for future debugging functionality

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"bufio"
+	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello! My name is Jocasta, and I'll be helping you with your data backups today.")
 
-	resp, err := http.Get("http://sample-aws-glacier-archive-url.com")
-	if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
+	response, error := http.Get("http://example.com")
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	fmt.Println("Response status:", response.Status)
+
+	scanner := bufio.NewScanner(response.Body)
+	for i := 0; scanner.Scan(); i++ {
+		fmt.Println(scanner.Text())
+	}
+	if error := scanner.Err(); error != nil {
+		panic(error)
+	}
 }
